3ca73718c095ac8914cb0dd8e02415f46063ea9a
AnonGhostID <32584610+AnonGhostID@users.noreply.github.com>
2024-11-14 12:51:48 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,8 +13,8 @@ log_ram_usage() {
         ram_usage_mb=$((ram_usage_kb / 1024))
         echo "RAM Usage: ${ram_usage_mb}MB"
         
-        if [ "$ram_usage_mb" -gt 230 ]; then
-            echo "RAM usage exceeded 230MB, restarting rclone"
+        if [ "$ram_usage_mb" -gt 256 ]; then
+            echo "RAM usage exceeded 256MB, restarting rclone"
             pkill rclone
             sleep 2 # Give some time for rclone to terminate
             eval "$CMD" &
